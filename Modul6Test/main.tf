72ff3eee815e84330851fc4253185c9829ccca21
RuneNorway <yobwons@online.no>
2024-10-07 18:46:00 +0200

    Tester med ny key

---
+++ Modul6Test/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.0.1"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-tfstate-rs"     # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "sabetfsrrj1iohywz"         # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                   # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "backendModul6test.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+
+}
+
+provider "azurerm" {
+  # Configuration options
+  subscription_id = "efc1e7b1-5729-4eea-b33e-12cc6b1c0183"
+
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg_backend" {
+  name     = var.rg_backend_name
+  location = var.rg_backend_location
+}
---
+++ Modul6Test/variables.tf
@@ -0,0 +1,8 @@
+variable "rg_backend_name" {
+  type        = string
+  description = "Resource group backend name"
+}
+variable "rg_backend_location" {
+  type        = string
+  description = "Resource group backend location"
+}
