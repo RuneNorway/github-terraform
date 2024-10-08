9b10721ff61b89aa5b753f3534edcad14ca32dd5
RuneNorway <yobwons@online.no>
2024-10-08 16:40:44 +0200

    Fjerner secret

--- Modul6Test/main.tf
+++ Modul6Test/main.tf
@@ -10,7 +10,7 @@ terraform {
     resource_group_name  = "rg-backend-tfstate-rs"     # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "sabetfsrrj1iohywz"         # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                   # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "backend.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key                  = "Modul6.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 
 }
