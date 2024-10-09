94eed44de3a8aa010551dacb7341e29fd9964313
RuneNorway <yobwons@online.no>
2024-10-09 20:31:40 +0200

    Ny test

--- Modul8/main.tf
+++ Modul8/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_resource_group" "rg" {
 
 }
 output "rg_name" {
-  value = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.rg.name 
 }
 
 # Create Storage account 
