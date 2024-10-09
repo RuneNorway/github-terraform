bb80419314831a5339606ff3b241c164299c64cf
RuneNorway <yobwons@online.no>
2024-10-09 20:27:00 +0200

    deploy web med actions

--- Modul8/variables.tf
+++ Modul8/variables.tf
@@ -2,26 +2,26 @@
 variable "rg_location" {
   type        = string
   description = "Resource group location"
-default = "westeurope"
+  default     = "westeurope"
 }
 variable "rg_name" {
   type        = string
   description = "Resource group name"
-default = "rg-mod8-rs"
+  default     = "rg-mod8-rs"
 }
 variable "sa_name" {
   type        = string
   description = "Resource group location"
-default = "samod8rs"
+  default     = "samod8rs"
 }
 variable "source_content" {
   type        = string
   description = "Source content for the index.html file"
-  default = "<h1>Web page created with Terraform in workspace:</h1>"
+  default     = "<h1>Web page created with Terraform in workspace:</h1>"
 }
 
 variable "index_document" {
   type        = string
   description = "Name for the index document"
-  default = "index.html"
+  default     = "index.html"
 }
